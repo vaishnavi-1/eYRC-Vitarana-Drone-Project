14bdc1861af74273e8b0dbf7eba4e3c707a1c437
Vaishnavi Saxena <vaishnavisaxena15@gmail.com>
2026-05-07 14:17:27 +0530

    Update Task_1_VD_0583_position_controller.py

--- task1_submission/Task_1_VD_0583_position_controller.py
+++ task1_submission/Task_1_VD_0583_position_controller.py
@@ -5,8 +5,7 @@ E-yantra
 Theme: Vitran Drone
 Task: task1B
 Purpose: Position controller
-Team ID : 0583
-Team name : !ABHIMANYU 
+
 **********************************'''
 
 # Importing the required libraries
